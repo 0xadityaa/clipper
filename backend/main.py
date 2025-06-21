8f6a8addc2b0587f8c5c2f6d605ec130f65d8db2
0xadityaa <negandhi.aditya@gmail.com>
2025-06-21 18:21:01 -0400

    WIP: added whisperx modals for video transcription and alignment with timestamps

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,15 @@
+import json
 import os
 import pathlib
+import subprocess
+import time
 import uuid
 import boto3
 from fastapi import Depends, HTTPException, status
 from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
 from pydantic import BaseModel
 import modal
+import whisperx
 
 
 class ProcessVideoRequest(BaseModel):
@@ -35,7 +39,35 @@ class Clipper:
     @modal.enter()
     def load_model(self):
         print("Loading models")
-        pass
+        self.whisperx_model = whisperx.load_model(
+            "large-v2", device="cuda", compute_type="float16")
+
+        self.alignment_model, self.metadata = whisperx.load_align_model(
+            language_code="en",
+            device="cuda"
+        )
+
+        print("Transcription models loaded...")
+
+    def transcribe_video(self, base_dir: str, video_path: str) -> str:
+        audio_path = base_dir / "audio.wav"
+        extract_cmd = f"ffmpeg -i {video_path} -vn -acodec pcm_s16le -ar 16000 -ac 1 {audio_path}"
+        subprocess.run(extract_cmd, shell=True,
+                       check=True, capture_output=True)
+
+        print("Starting transcription with WhisperX...")
+        start_time = time.time()
+
+        audio = whisperx.load_audio(str(audio_path))
+        result = self.whisperx_model.transcribe(audio, batch_size=16)
+
+        result = whisperx.align(
+            result["segments"], self.alignment_model, self.metadata, audio, device="cuda", return_char_alignments=False)
+
+        duration = time.time() - start_time
+        print("Transcription and alignment took: " + str(duration) + " seconds")
+
+        print(json.dumps(result, indent=2))
 
     @modal.fastapi_endpoint(method="POST")
     def process_video(self, request: ProcessVideoRequest, token: HTTPAuthorizationCredentials = Depends(auth_scheme)):
@@ -44,7 +76,7 @@ class Clipper:
         if token.credentials != os.environ["AUTH_TOKEN"]:
             raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
                                 detail="Incorrect bearer token", headers={"WWW-Authenticate": "Bearer"})
-        
+
         run_id = str(uuid.uuid4())
         base_dir = pathlib.Path("/tmp") / run_id
         base_dir.mkdir(parents=True, exist_ok=True)
@@ -53,6 +85,8 @@ class Clipper:
         video_path = base_dir / "short.mp4"
         s3_client = boto3.client("s3")
         s3_client.download_file("clipper-bucket01", s3_key, str(video_path))
+
+        self.transcribe_video(base_dir, str(video_path))
 
         print(os.listdir(base_dir))
 
